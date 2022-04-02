666e6aec15cd2dafe925ace245c84469707e6cd5
fack <4878248+facktheplanet@users.noreply.github.com>
2022-04-01 22:46:56 -0500

    coding room floor

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,17 @@ fn main() {
     let path = Path::new("links.txt");
     let line_num = 6usize;
     let new_content = "new content";
+    append_line(path, new_content);
     let line = get_line_at(&path, line_num - 1);
-    let 
+    let last_line = get_last_line(&path);
+    let last_content = get_line_at(&path, last_line - 1);
     println!("{}", line.unwrap());
-}
+    println!("{}", last_line);
+    println!("{}", last_content.as_ref().unwrap());
+
+    println!("the last line is {} containing the following content {}", last_line, last_content.unwrap());
  
+}
 fn get_line_at(path: &Path, line_num: usize) -> Result<String, Error> {
     let file = File::open(path).expect("File not found or cannot be opened");
     let content = BufReader::new(&file);
@@ -23,13 +29,15 @@ fn get_line_at(path: &Path, line_num: usize) -> Result<String, Error> {
     lines.nth(line_num).expect("No line found at that position")
 }
 
-fn append(path: &Path, new_content: &str) -> Result<usize, Error> {
+fn append_line(path: &Path, new_content: &str) -> std::io::Result<()> {
     let mut file = OpenOptions::new().append(true).open(path).expect("File not found or cannot be opened");
-    //fs::write(&file, new_content)?;
-    write!(file, "{}", new_content)?;
+    write!(file, "{}\n", new_content);
+    Ok(())
+}
 
+fn get_last_line(path: &Path) -> usize {
+    let mut file = OpenOptions::new().read(true).open(path).expect("File not found or cannot be opened");
     let content = BufReader::new(&file);
-    let mut lines = content.lines();
-    return Ok(lines.count());
-
+    let lines = content.lines();
+    return lines.count();
 }
